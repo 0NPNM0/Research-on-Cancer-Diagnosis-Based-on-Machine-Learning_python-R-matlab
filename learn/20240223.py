249556ffbaba464b3b588be3fe46e952e42e83e5
0NPNM0 <2661156778@qq.com>
2024-02-26 22:01:27 +0800

    1

--- learn/20240223.py
+++ learn/20240223.py
@@ -1,6 +1,6 @@
 # import pydicom as dicom
 #
-# dcm = dicom.read_file("/home/npnm/Desktop/Research-on-Cancer-Diagnosis-Based-on-Machine-Learning_python/Data/LIDC-IDRI-0001/01-01-2000-NA-NA-30178/3000566.000000-NA-03192/1-006.dcm")
+# dcm = dicom.read_file("/home/npnm/Desktop/Research-on-Cancer-Diagnosis-Based-on-Machine-Learning_python/Data Download/LIDC-IDRI-0001/01-01-2000-NA-NA-30178/3000566.000000-NA-03192/1-006.dcm")
 # print(dcm)
 
 from ucimlrepo import fetch_ucirepo
